013619a03d1bd7e28c94d2058e5dcc9ad1ee477f
Soe Thiha Tun <5600819+soethihatun@users.noreply.github.com>
2024-05-11 21:41:08 +0900

    EA-1 Add :feature:home module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,7 @@ android {
 dependencies {
 
     implementation(project(":core:designsystem"))
+    implementation(project(":feature:home"))
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)

4aaf1dec393a489586ea9586195dc43af412ba3a
Soe Thiha Tun <5600819+soethihatun@users.noreply.github.com>
2024-05-11 13:12:12 +0900

    EA-1 Add :core:designsystem module

--- core/designsystem/build.gradle.kts
+++ core/designsystem/build.gradle.kts
@@ -1,29 +1,22 @@
 plugins {
-    alias(libs.plugins.android.application)
+    alias(libs.plugins.android.library)
     alias(libs.plugins.jetbrains.kotlin.android)
 }
 
 android {
-    namespace = "co.binary.exploregithubandroid"
+    namespace = "co.binary.exploregithubandroid.core.designsystem"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "co.binary.exploregithubandroid"
         minSdk = 26
-        targetSdk = 34
-        versionCode = 1
-        // Major.Minor.Patch
-        versionName = "0.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        vectorDrawables {
-            useSupportLibrary = true
-        }
+        consumerProguardFiles("consumer-rules.pro")
     }
 
     buildTypes {
         release {
-            isMinifyEnabled = true
+            isMinifyEnabled = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
@@ -34,26 +27,11 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
-    buildFeatures {
-        compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
-    }
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
-    }
 }
 
 dependencies {
 
-    implementation(project(":core:designsystem"))
-
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,4 +21,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "Explore GitHub Android"
 include(":app")
- 
+include(":core:designsystem")

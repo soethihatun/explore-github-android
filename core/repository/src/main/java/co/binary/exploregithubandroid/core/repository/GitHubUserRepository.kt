6d7b68c01128db1dbe616fe64c848dd0cfd6582b
Soe Thiha Tun <5600819+soethihatun@users.noreply.github.com>
2024-05-11 20:58:04 +0900

    EA-1 Add GitHubUserRepository

---
+++ core/repository/src/main/java/co/binary/exploregithubandroid/core/repository/GitHubUserRepository.kt
@@ -0,0 +1,10 @@
+package co.binary.exploregithubandroid.core.repository
+
+import co.binary.exploregithubandroid.core.model.GitHubUser
+import co.binary.exploregithubandroid.core.model.GitHubUserDetail
+
+interface GitHubUserRepository {
+    suspend fun searchGitHubUser(query: String): Result<List<GitHubUser>>
+
+    suspend fun getGitHubUserDetail(login: String): Result<GitHubUserDetail>
+}

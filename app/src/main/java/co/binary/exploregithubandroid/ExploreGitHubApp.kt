76799c1432bb747d88b854b2a601c1c130eb4d24
Soe Thiha Tun <5600819+soethihatun@users.noreply.github.com>
2024-05-11 20:41:58 +0900

    EA-1 Add ExploreGitHubApp

---
+++ app/src/main/java/co/binary/exploregithubandroid/ExploreGitHubApp.kt
@@ -0,0 +1,12 @@
+package co.binary.exploregithubandroid
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ExploreGitHubApp: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
--- app/src/main/java/co/binary/exploregithubandroid/MainActivity.kt
+++ app/src/main/java/co/binary/exploregithubandroid/MainActivity.kt
@@ -12,7 +12,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import co.binary.exploregithubandroid.core.designsystem.theme.ExploreGitHubAndroidTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)

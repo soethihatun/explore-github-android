817cf2c061df67d328686e1af43036090c776552
Soe Thiha Tun <5600819+soethihatun@users.noreply.github.com>
2024-05-13 18:19:03 +0900

    EA-1 Update ExploreGitHubApp

--- app/src/main/java/co/binary/exploregithubandroid/ExploreGitHubApp.kt
+++ app/src/main/java/co/binary/exploregithubandroid/ExploreGitHubApp.kt
@@ -1,13 +1,25 @@
 package co.binary.exploregithubandroid
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
 
 @Composable
-internal fun ExploreGitHubApp(modifier: Modifier = Modifier) {
-    ExploreGitHubNavHost(
-        modifier = modifier,
-        navController = rememberNavController(),
-    )
+internal fun ExploreGitHubApp() {
+    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+        ) {
+            ExploreGitHubNavHost(
+                modifier = Modifier.fillMaxSize(),
+                navController = rememberNavController(),
+            )
+        }
+    }
 }
